0841d5ceb0d09d9fca0cc78674ef0e23d0ff54ba
andbri <andbri312@gmail.com>
2019-10-26 19:10:07 -0300

    [c] model Monster

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_26_204111) do
+ActiveRecord::Schema.define(version: 2019_10_26_220856) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,12 @@ ActiveRecord::Schema.define(version: 2019_10_26_204111) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_collected_coins_on_user_id"
+  end
+
+  create_table "monsters", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
