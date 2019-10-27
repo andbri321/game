dc53f549dfc129c0780a7cda864a42fdf64cebb6
andbri <andbri312@gmail.com>
2019-10-27 17:41:17 -0300

    [u] model KilledMonster

--- app/models/killed_monster.rb
+++ app/models/killed_monster.rb
@@ -1,4 +1,20 @@
 class KilledMonster < ApplicationRecord
   belongs_to :user
   belongs_to :monster
+
+  after_save :increment_killed_monsters_count
+  after_destroy :decrement_killed_monsters_count
+
+  private
+
+  def increment_killed_monsters_count
+    User.increment_counter(
+        "#{self.monster.name.downcase}_killed_monsters_count", user_id)
+  end
+
+  def decrement_killed_monsters_count
+    User.decrement_counter(
+        "#{self.monster.name.downcase}_killed_monsters_count", user_id)
+  end
+
 end
