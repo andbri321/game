f93a863ad91d842ab8a14ea33c7ca060dc6844eb
andbri <andbri312@gmail.com>
2019-10-31 12:25:31 -0300

    [u] routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'deaths/index'
   get 'collected_coins/index'
   post 'collected_coins/create'
+  post 'deaths/create'
 
   get 'home/index'
   devise_for :users
