04626567d128f8321d25f3684478aeb7abf599d9
andbri <andbri312@gmail.com>
2019-10-31 12:24:53 -0300

    [c] controller Deaths

---
+++ app/controllers/deaths_controller.rb
@@ -0,0 +1,21 @@
+class DeathsController < ApplicationController
+  before_action :set_deaths_count, only: [:index,:create]
+
+  def index
+    @deaths = current_user.deaths.new
+  end
+
+  def create
+    @deaths =  current_user.deaths.create()
+    unless @deaths.errors.any?
+      redirect_to deaths_index_path
+    else
+      render :index
+    end
+  end
+
+  def set_deaths_count
+    @deaths_count = current_user.deaths_count
+  end
+
+end
