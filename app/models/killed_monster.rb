dc132ee61b6b3939dac996e6bd03a6addc8d9c24
andbri <andbri312@gmail.com>
2019-11-01 14:10:24 -0300

    [u] KilledMonsters,add callback set_trophy

--- app/models/killed_monster.rb
+++ app/models/killed_monster.rb
@@ -1,9 +1,12 @@
 class KilledMonster < ApplicationRecord
+  include CallBackShared
+
   belongs_to :user
   belongs_to :monster
 
   after_save :increment_killed_monsters_count
   after_destroy :decrement_killed_monsters_count
+  after_save ->(obj) { set_trophy(user_id) }
 
   private
 
