447272f775fbd204e046101e6e9757377b5b7ac0
andbri <andbri312@gmail.com>
2019-11-01 14:11:01 -0300

    [u] CollectedCoin,add callback set_trophy

--- app/models/collected_coin.rb
+++ app/models/collected_coin.rb
@@ -1,6 +1,9 @@
 class CollectedCoin < ApplicationRecord
+  include CallBackShared
+
   belongs_to :user, :counter_cache => true
 
   validates :value, presence: true, numericality: true
+  after_save ->(obj) { set_trophy(user_id) }
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_26_225404) do
+ActiveRecord::Schema.define(version: 2019_10_29_235212) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,9 @@ ActiveRecord::Schema.define(version: 2019_10_26_225404) do
     t.datetime "updated_at", null: false
     t.integer "deaths_count", default: 0, null: false
     t.integer "collected_coins_count", default: 0, null: false
+    t.integer "turtle_killed_monsters_count", default: 0, null: false
+    t.integer "bowser_killed_monsters_count", default: 0, null: false
+    t.integer "trophy", default: 0, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
